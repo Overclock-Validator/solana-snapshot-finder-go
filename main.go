fdda427416de55c0626e9993f4c99d22b03faaec
Chuck Veenvliet <cveenvliet@blockdaemon.com>
2025-01-20 14:59:05 +0100

    chore(): added tags

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
+
+var version string
 
 func main() {
 	// Parse command-line arguments
 	configPath := flag.String("config", "./config.yaml", "Path to the configuration file directory")
+	versionFlag := flag.Bool("version", false, "Show version information")
 	flag.Parse()
+
+	// If version flag is passed, print the version and exit
+	if *versionFlag {
+		if version == "" {
+			version = "unknown" // In case version is not set, fallback to 'unknown'
+		}
+		fmt.Printf("Solana Snapshot Finder, Version: %s\n", version)
+		os.Exit(0)
+	}
 
 	// Load the configuration
 	config, err := LoadConfig(*configPath)
